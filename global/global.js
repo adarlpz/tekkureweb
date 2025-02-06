d29d9b5e8db0c358003fb22dfe6a10aeff142781
Emilio-Madrigal <madrigalemi31@gmail.com>
2025-02-06 16:17:35 -0600

    Co-authored-by: Brandon Lopez <adarlopz.dev@gmail.com>

--- global/global.js
+++ global/global.js
@@ -1,2 +1,23 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('year').textContent = currentYear;
+
+document.addEventListener("DOMContentLoaded", function () {
+    cargarContenido("global.html", "header-container", "header");
+    cargarContenido("global.html", "footer-container", "footer");
+});
+
+function cargarContenido(url, contenedorId, selector) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            // Crear un elemento temporal para extraer solo el header o footer
+            let tempDiv = document.createElement("div");
+            tempDiv.innerHTML = data;
+
+            let elemento = tempDiv.querySelector(selector);
+            if (elemento) {
+                document.getElementById(contenedorId).innerHTML = elemento.outerHTML;
+            }
+        })
+        .catch(error => console.error("Error cargando " + selector, error));
+}
