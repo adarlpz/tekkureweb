d54780afd189a7e00ced4ce69bcdab15dd84aca2
adarlpz <adarlopz.dev@gmail.com>
2025-02-07 10:46:21 -0600

    pruieba links

--- global/global.js
+++ global/global.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     cargarContenido("/global/global.html", "header-container", "header");
     cargarContenido("/global/global.html", "footer-container", "footer");
+    cargarLinksGlobales("/global/global.html");
 
     setTimeout(() => {
         document.getElementById("year").textContent = new Date().getFullYear();
@@ -19,4 +20,20 @@ function cargarContenido(url, contenedorId, selector) {
             }
         })
         .catch(error => console.error("Error cargando " + selector, error));
+}
+function cargarLinksGlobales(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            let tempDiv = document.createElement("div");
+            tempDiv.innerHTML = data;
+            let links = tempDiv.querySelectorAll('link[rel="stylesheet"], link[rel="preconnect"], link[rel="shortcut icon"]');
+
+            links.forEach(link => {
+                if (!document.head.querySelector(`link[href="${link.href}"]`)) {
+                    document.head.appendChild(link.cloneNode(true)); // Agregar solo si no existe
+                }
+            });
+        })
+        .catch(error => console.error("Error cargando links globales", error));
 }
